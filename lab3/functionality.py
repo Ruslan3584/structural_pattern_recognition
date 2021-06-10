d4e8a390baaf3ea230f62810cb0ef087596a839b
Ruslan Khomenko <37312893+Ruslan3584@users.noreply.github.com>
2021-06-10 22:04:37 +0300

    update lab3, add new images

--- lab3/functionality.py
+++ lab3/functionality.py
@@ -1,7 +1,8 @@
 import numpy as np
 from numpy import ndarray
 from numpy.linalg import norm
-import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
+from scipy.stats import multivariate_normal
 from numba import njit
 
 @njit
@@ -21,78 +22,62 @@ def get_neighbours(height,width,i,j):
         N: tuple
             neighbours coordinates, neighbours indices, inverse neighbours indices
     calculates neighbours in 4-neighbours system (and inverse indices)
-    
-    
-             (i-1,j-1)  (i-1,j)  (i-1,j+1) 
-                      \    |    /
-                       \   |   /
-                        \  |  /
-                (i,j-1)--(i,j)--(i,j+1)
-                        /  |  \
-                       /   |   \
-                      /    |    \
-             (i+1,j-1)  (i+1,j)  (i+1,j+1)
+            (i-1,j)
+               |
+    (i,j-1)--(i,j)--(i,j+1)
+               |
+            (i+1,j)
+    examples:
+    >>> get_neighbours(-1,1,1,1)
+    Traceback (most recent call last):
+    ...
+    Exception: height or width is less than zero
+    >>> get_neighbours(3,3,-1,-1)
+    ([], [], [])
+    >>> get_neighbours(3,3,0,0)
+    ([[0, 1], [1, 0]], [0, 2], [1, 3])
+    >>> get_neighbours(3,3,0,1)
+    ([[0, 0], [0, 2], [1, 1]], [1, 0, 2], [0, 1, 3])
+    >>> get_neighbours(3,3,1,1)
+    ([[1, 0], [1, 2], [0, 1], [2, 1]], [1, 0, 3, 2], [0, 1, 2, 3])
     '''
+
+    if width <= 0 or height <= 0:
+        raise Exception('height or width is less than zero')
 
     # i,j - position of pixel
     # [Left, Right, Up, Down] - order of possible neighbours
     # array of neighbour indices
-
-    nbs = []
+    nbs = [] 
     # neighbour indices
     nbs_indices = []
     # inverse neighbour indices
     inv_nbs_indices = []
-    # Top Left
-    if 0<=i-1<height-1 and 0<=j-1<width-1:
-        nbs.append([i-1,j-1])
-        inv_nbs_indices.append(7)
+    # Left
+    if 0<=j-1<width-1 and 0<=i<=height-1:
+        nbs.append([i,j-1])
+        inv_nbs_indices.append(1)
         nbs_indices.append(0)
-        
-    # Top Up
+    # Right
+    if 0<j+1<=width-1 and 0<=i<=height-1:
+        nbs.append([i,j+1])
+        inv_nbs_indices.append(0)
+        nbs_indices.append(1)
+    # Upper
     if 0<=i-1<height-1 and 0<=j<=width-1:
         nbs.append([i-1,j])
-        inv_nbs_indices.append(6)
-        nbs_indices.append(1)        
-
-    # Top Right
-    if 0<=i-1<height-1 and 0<j+1<=width-1:
-        nbs.append([i-1,j+1])
-        inv_nbs_indices.append(5)
-        nbs_indices.append(2)
-    # Left
-    if 0<=i<=height-1 and 0<=j-1<width-1:
-        nbs.append([i,j-1])
-        inv_nbs_indices.append(4)
-        nbs_indices.append(3)
-    # Right
-    if 0<=i<=height-1 and 0<j+1<=width-1:
-        nbs.append([i,j+1])
         inv_nbs_indices.append(3)
-        nbs_indices.append(4)
-    # Bottom Left
-    if 0<i+1<=height-1 and 0<=j-1<width-1:
-        nbs.append([i+1,j-1])
-        inv_nbs_indices.append(2)
-        nbs_indices.append(5)
-    # Bottom Down
+        nbs_indices.append(2)
+    # Down
     if 0<i+1<=height-1 and 0<=j<=width-1:
         nbs.append([i+1,j])
-        inv_nbs_indices.append(1)
-        nbs_indices.append(6)
-    # Bottom Right
-    if 0<i+1<=height-1 and 0<j+1<=width-1:
-        nbs.append([i+1,j+1])
-        inv_nbs_indices.append(0)
-        nbs_indices.append(7)
-    
+        inv_nbs_indices.append(2)
+        nbs_indices.append(3)
     N = (nbs, inv_nbs_indices, nbs_indices)
     return N
 
-
 @njit(fastmath=True, cache=True)
 def diff_iter(height: int, width: int, fi: ndarray, g:ndarray, K:ndarray, Q:ndarray) -> ndarray:
-
     '''
     Parameters
         height: int
@@ -110,9 +95,9 @@ def diff_iter(height: int, width: int, fi: ndarray, g:ndarray, K:ndarray, Q:ndar
     Returns
         fi: ndarray
             updated array of potentials
-
     calculates one iteration of diffusion 
     '''
+    
     # for each pixel of input image
     for i in range(height):
         for j in range(width):
@@ -120,20 +105,20 @@ def diff_iter(height: int, width: int, fi: ndarray, g:ndarray, K:ndarray, Q:ndar
             nbs, inv_nbs_indices, nbs_indices = get_neighbours(height, width,i,j)
             len_neighbours = len(nbs)
             k_asterisk = np.full(len_neighbours, -1)
-            fi_list = np.full(len_neighbours, 0)
-            C_t = np.full(len_neighbours, 0)
+            fi_list = np.full(len_neighbours, np.nan)
+            C_t = np.full(len_neighbours, np.nan)
             # for each label in label set
             for k in K:
                 # for each neighbour of pixel (i,j)
                 for n,[n_i,n_j] in enumerate(nbs):
                     # k*(t`)
-                    k_asterisk[n] = np.argmax(g[i,j,n,k,:] - fi[n_i,n_j,inv_nbs_indices[n],:])
+                    k_asterisk[n] = np.argmax(g[i,j,nbs_indices[n],k,:] - fi[n_i,n_j,inv_nbs_indices[n],:])
                     # fi(t',t)(k*)
                     fi_list[n] = fi[n_i,n_j,inv_nbs_indices[n],k_asterisk[n] ]
-                    if g[i,j,n,k,k_asterisk[n]] == -np.inf:
+                    if g[i,j,nbs_indices[n],k,k_asterisk[n]] == -np.inf:
                         C_t[n] = -np.inf
                     else:
-                        C_t[n] = g[i,j,n,k,k_asterisk[n]] - fi_list[n]
+                        C_t[n] = g[i,j,nbs_indices[n],k,k_asterisk[n]] - fi_list[n]
                 #print(C_t)
                 C_t_sum = (np.sum(C_t) + Q[i,j,k])/len_neighbours
                 # updating potentials
@@ -142,7 +127,6 @@ def diff_iter(height: int, width: int, fi: ndarray, g:ndarray, K:ndarray, Q:ndar
     return fi
 
 def diffusion(height,width,K,Q,g, n_iter):
-
     '''
     Parameters
         height: int
@@ -160,7 +144,6 @@ def diffusion(height,width,K,Q,g, n_iter):
     Returns
         fi: ndarray
             updated array of potentials after n iterations
-
     n iterations of diffusion
     examples:
     >>> diffusion('height','width','K','Q','g', 0)
@@ -168,18 +151,45 @@ def diffusion(height,width,K,Q,g, n_iter):
     ...
     Exception: number of iterations is less or equal to zero
     '''
-
     if n_iter <= 0:
         raise Exception('number of iterations is less or equal to zero')
-
+        
     n_labels = len(K)
-    # 8 system neighbourhood
-    n_neighbors = 8
+    n_neighbors = 4
     # initialize potentials as zeros
     fi = np.zeros((height,width,n_neighbors,n_labels))
     for i in range(n_iter):
         fi = diff_iter(height,width,fi,g,K,Q)
     return fi
+
+def update_fi(height,width,fi,g,K,Q,n_iter):
+    '''
+    Parameters
+        height: int
+            height of input image 
+        width: int
+            width of input image 
+        fi: ndarray
+            array of potentials
+        g: ndarray
+            array of binary penalties
+        K: ndarray
+            label set
+        Q: ndarray
+            array of unary penalties
+        n_iter: int
+            number of iteration
+    Returns
+        fi: ndarray
+            updated array of potentials
+    updates fi after n_iter iterations
+    '''
+    n_labels = len(K)
+    n_neighbors = 4
+    for i in range(n_iter):
+        fi = diff_iter(height,width,fi,g,K,Q)
+    return fi
+
 
 def get_labelling(height, width, g, fi):
 
@@ -191,8 +201,6 @@ def get_labelling(height, width, g, fi):
             width of input image 
         g: ndarray
             array of binary penalties
-        c: ndarray
-            color mapping
         fi: ndarray
             updated array of potentials
     Returns
@@ -201,16 +209,46 @@ def get_labelling(height, width, g, fi):
 
     labelling restoration after diffusion
     '''
-    c = np.array([[0,0,255],[255,0,0],[0,255,0]])
-    labelling = np.empty((height,width,len(c[0])),dtype = int)
+    labelling = np.empty((height,width),dtype = int)
     for i in range(height):
         for j in range(width):
             nbs, inv_nbs_indices, nbs_indices = get_neighbours(height, width,i,j)
-            # take any neighbour
             n_i, n_j = nbs[0]
             # calculating reparametrized binary penalties
-            g_reparametrized = g[i,j,nbs_indices[0],:] - fi[i,j,nbs_indices[0],:] - fi[n_i,n_j,inv_nbs_indices[0],:]
+            g_reparametrized = g[i,j,nbs_indices[0],...] - fi[i,j,nbs_indices[0],...] - fi[n_i,n_j,inv_nbs_indices[0],...]
             # g - is supermodular so take the highest possible maximum edge between nodes t, t'
-            labelling[i,j,:] = c[np.argmax(np.max(g_reparametrized,axis = 0))]
+            labelling[i,j] = np.argmax(np.max(g_reparametrized,axis = 0))
     return labelling
 
+
+def get_gmm_params(img, n_rows):
+    width = img.shape[1]
+    class0 = GaussianMixture(n_components=1).fit(img.reshape(-1,3)[:(n_rows*width)] )
+    class1 = GaussianMixture(n_components=1).fit(img.reshape(-1,3)[(n_rows*width):] )
+    return class0.means_[0], class0.covariances_[0], class1.means_[0],  class1.covariances_[0]
+
+def get_q(img, mean0, cov0, mean1, cov1):
+    height, width = img.shape[:2]
+    K = np.array([0,1])
+    mask = multivariate_normal.pdf(img, mean0, cov0) > multivariate_normal.pdf(img, mean1, cov1)
+    q = np.zeros((height,width, 2))
+    q[mask] = (K != 1)
+    q[~mask] = (K != 0)
+    return -q
+
+def get_g(height,width,n_labels,beta):
+    g = np.zeros((height,width, 4, 2, 2))
+    g[..., 0, 1] = -beta
+    g[..., 1, 0] = -beta
+    return g
+
+@njit
+def check_smooth_border(mask, height, width):
+    for i in range(height-1):
+        for j in range(width):
+            if (not mask[i, j]) and (not mask[i+1, j]):
+                if  j != 0 and mask[i, j-1] and mask[i+1, j-1]:
+                    return False
+                if  j < width - 1 and mask[i, j+1] and mask[i+1, j+1]:
+                    return False
+    return True
