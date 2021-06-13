a8c0b1ed49a06ef8bdec0ee15e157b032c5d2542
Ruslan Khomenko <37312893+Ruslan3584@users.noreply.github.com>
2021-06-13 19:26:59 +0300

    fix bug with labeling

--- lab3/functionality.py
+++ lab3/functionality.py
@@ -246,9 +246,131 @@ def get_g(height,width,n_labels,beta):
 def check_smooth_border(mask, height, width):
     for i in range(height-1):
         for j in range(width):
+            if (not mask[i, j]) and mask[i + 1, j] and (mask[i+1:, j] == 0).any():
+                    return False
             if (not mask[i, j]) and (not mask[i+1, j]):
                 if  j != 0 and mask[i, j-1] and mask[i+1, j-1]:
                     return False
                 if  j < width - 1 and mask[i, j+1] and mask[i+1, j+1]:
                     return False
     return True
+
+
+
+
+@njit
+def get_reparametrized_q(q, fi):
+    height, width, n_labels = q.shape
+    q_reparametrized = np.copy(q).astype(np.float64)
+    for i in range(height):
+        for j in range(width):
+            nbs, inv_nbs_indices, nbs_indices = get_neighbours(height,width,i,j)
+            for k in range(n_labels):
+                for n,[n_i,n_j] in enumerate(nbs):
+                    q_reparametrized[i,j,k] += fi[i,j,nbs_indices[n],k]
+    return q_reparametrized
+
+
+
+@njit
+def get_reparametrized_g(g,fi):
+    g_reparametrized = np.full_like(g, -np.inf)
+    eps_array = np.zeros_like(g)
+    for i in range(height):
+        for j in range(width):
+            nbs, inv_nbs_indices, nbs_indices = get_neighbours(height,width,i,j)
+            for n,[n_i,n_j] in enumerate(nbs):
+                # calculating reparametrized binary penalties
+                for k in range(n_labels):
+                    for k_prime in range(n_labels):
+                        g_reparametrized[i,j,nbs_indices[n],k,k_prime] = g[i,j,nbs_indices[n],k,k_prime] - fi[i,j,nbs_indices[n],k]  - fi[n_i,n_j,inv_nbs_indices[n],k_prime]
+                g_max = np.max(g_reparametrized[i,j,nbs_indices[n],...])
+                eps_array[i,j,nbs_indices[n],:] = np.abs(g_max - g_reparametrized[i,j,nbs_indices[n],...])
+                #print(np.unique(eps_array[i,j,nbs_indices[n],:])[-2])
+    eps_unique = np.unique(eps_array)
+    return g_reparametrized, eps_unique
+
+
+@njit
+def get_g_binary(g_reparametrized, epsilon):
+    height, width = g_reparametrized.shape[:2]
+    g_binary = np.full_like(g_reparametrized, 0)
+    for i in range(height):
+        for j in range(width):
+            nbs, inv_nbs_indices, nbs_indices = get_neighbours(height,width,i,j)
+            for n,[n_i,n_j] in enumerate(nbs):
+                    g_max = np.max(g_reparametrized[i,j,nbs_indices[n],:])
+                    g_binary[i,j,nbs_indices[n],:] = (np.abs(g_max - g_reparametrized[i,j,nbs_indices[n],:]) < epsilon)
+    return g_binary
+
+
+@njit
+def get_q_binary(q_reparametrized, epsilon):
+    height, width = q_reparametrized.shape[:2]
+    q_binary = np.full_like(q_reparametrized, 0)
+    for i in range(height):
+        for j in range(width):
+            q_max = np.max(q_reparametrized[i,j,:])
+            q_binary[i,j,:] =  (np.abs(q_max - q_reparametrized[i,j,:]) < epsilon)
+    return q_binary
+
+
+@njit
+def crossing_out(q, g):
+    height, width = g.shape[:2]
+    n_labels = g.shape[-1]
+    change_indicator = True
+    while(change_indicator):
+        change_indicator = False
+        for i in range(height):
+            for j in range(width):
+                nbs, inv_nbs_indices, nbs_indices = get_neighbours(height,width,i,j)
+                for k in range(n_labels):
+                    if q[i,j,k] == 0:
+                        for n,[n_i,n_j] in enumerate(nbs):
+                            if (g[i,j,nbs_indices[n],k,:] == 1).any():
+                                g[i,j,nbs_indices[n],k,:] = 0
+                                g[n_i,n_j,inv_nbs_indices[n],:,k] = 0
+                                change_indicator = True
+                    elif q[i,j,k] == 1:
+                        exists = 0
+                        edge_exists = False
+                        for n,[n_i,n_j] in enumerate(nbs):
+                            for k1 in range(n_labels):
+                                if g[i,j,nbs_indices[n],k,k1] == 1 and q[n_i,n_j,k1] == 1:
+                                    edge_exists = True
+                                    break
+                            if edge_exists == True:
+                                exists += 1
+                        if exists != len(nbs):
+                            for n,[n_i,n_j] in enumerate(nbs):
+                                if (g[i,j,nbs_indices[n],k,:]).any() == 1:
+                                    g[i,j,nbs_indices[n],k,:] = 0
+                                    g[n_i,n_j,inv_nbs_indices[n],:,k] = 0
+                                    change_indicator = True
+                                if q[i,j,k] == 1:
+                                    q[i,j,k] = 0
+                                    change_indicator = True
+        if (q == 0).all() or (g == 0).all():
+            print("ALL ZEROS")
+            return 0
+    return 1
+
+@njit
+def self_control(q_binary,g_binary):
+    height,width,n_labels = q_binary.shape
+    result = np.zeros((height,width))
+    for i in range(height):
+        for j in range(width):
+            for k in range(n_labels):
+                if q_binary[i,j,k] == 1:
+                    q_binary[i,j,:] = 0
+                    q_binary[i,j,k] = 1
+                    flag = crossing_out(q_binary, g_binary)
+                    if flag == 1:
+                        result[i,j] = k
+                        break
+                    if k == n_labels - 1 and flag == 0:
+                        print('bad parameters')
+    return result
+
